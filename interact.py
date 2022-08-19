942bf0e10aad77251a5ae1b2dc3c0694c4904adc
kaj <40004347+KAJdev@users.noreply.github.com>
2022-08-19 15:26:09 -0800

    fix

--- interact.py
+++ interact.py
@@ -1,11 +1,13 @@
 import scripts.txt2img as sampler
+from omegaconf import OmegaConf
 
 if __name__ == "__main__":
 
-    print("Loading models, hold on...")
-    model = sampler.load_model_from_config("configs/stable-diffusion/v1-inference.yaml", "models/ldm/stable-diffusion-v1/model.ckpt")
+    print("Starting model loading...")
+    config = OmegaConf.load("configs/stable-diffusion/v1-inference.yaml")
+    model = sampler.load_model_from_config(config, "models/ldm/stable-diffusion-v1/model.ckpt")
 
-    print("Welcome to stable diffusion interact prompt mode! ENter your prompt to generate a sample!")
+    print("Welcome to stable diffusion interact prompt mode! Enter your prompt to generate a sample!")
     while True:
         prompt = input(">> ")
 
