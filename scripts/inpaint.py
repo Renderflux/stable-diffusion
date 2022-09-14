3b38e8eaebab7d92be7104bc2eb407bf597bc7eb
kaj <40004347+KAJdev@users.noreply.github.com>
2022-09-14 09:24:14 -0800

    feat: functional inpainting

--- scripts/inpaint.py
+++ scripts/inpaint.py
@@ -29,6 +29,53 @@ def make_batch(image, mask, device):
         batch[k] = batch[k]*2.0-1.0
     return batch
 
+def sample(
+    mask,
+    image,
+    steps,
+    model=None
+):
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    config = OmegaConf.load("configs/stable-diffusion/v1-inference.yaml")
+    if model is None:
+        model = instantiate_from_config(config.model)
+        model.load_state_dict(torch.load("models/ldm/stable-diffusion-v1/model.ckpt")["state_dict"],
+                            strict=False)
+        model = model.to(device)
+
+    sampler = DDIMSampler(model)
+
+    os.makedirs(opt.outdir, exist_ok=True)
+    with torch.no_grad():
+        with model.ema_scope():
+            outpath = os.path.join(opt.outdir, os.path.split(image)[1])
+            batch = make_batch(image, mask, device=device)
+
+            # encode masked image and concat downsampled mask
+            c = model.cond_stage_model.encode(batch["masked_image"])
+            cc = torch.nn.functional.interpolate(batch["mask"],
+                                                    size=c.shape[-2:])
+            c = torch.cat((c, cc), dim=1)
+
+            shape = (c.shape[1]-1,)+c.shape[2:]
+            samples_ddim, _ = sampler.sample(S=opt.steps,
+                                                conditioning=c,
+                                                batch_size=c.shape[0],
+                                                shape=shape,
+                                                verbose=False)
+            x_samples_ddim = model.decode_first_stage(samples_ddim)
+
+            image = torch.clamp((batch["image"]+1.0)/2.0,
+                                min=0.0, max=1.0)
+            mask = torch.clamp((batch["mask"]+1.0)/2.0,
+                                min=0.0, max=1.0)
+            predicted_image = torch.clamp((x_samples_ddim+1.0)/2.0,
+                                            min=0.0, max=1.0)
+
+            inpainted = (1-mask)*image+mask*predicted_image
+            inpainted = inpainted.cpu().numpy().transpose(0,2,3,1)[0]*255
+            Image.fromarray(inpainted.astype(np.uint8)).save(outpath)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -56,43 +103,9 @@ if __name__ == "__main__":
     images = [x.replace("_mask.png", ".png") for x in masks]
     print(f"Found {len(masks)} inputs.")
 
-    config = OmegaConf.load("models/ldm/inpainting_big/config.yaml")
-    model = instantiate_from_config(config.model)
-    model.load_state_dict(torch.load("models/ldm/inpainting_big/last.ckpt")["state_dict"],
-                          strict=False)
-
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model = model.to(device)
-    sampler = DDIMSampler(model)
-
-    os.makedirs(opt.outdir, exist_ok=True)
-    with torch.no_grad():
-        with model.ema_scope():
-            for image, mask in tqdm(zip(images, masks)):
-                outpath = os.path.join(opt.outdir, os.path.split(image)[1])
-                batch = make_batch(image, mask, device=device)
-
-                # encode masked image and concat downsampled mask
-                c = model.cond_stage_model.encode(batch["masked_image"])
-                cc = torch.nn.functional.interpolate(batch["mask"],
-                                                     size=c.shape[-2:])
-                c = torch.cat((c, cc), dim=1)
-
-                shape = (c.shape[1]-1,)+c.shape[2:]
-                samples_ddim, _ = sampler.sample(S=opt.steps,
-                                                 conditioning=c,
-                                                 batch_size=c.shape[0],
-                                                 shape=shape,
-                                                 verbose=False)
-                x_samples_ddim = model.decode_first_stage(samples_ddim)
-
-                image = torch.clamp((batch["image"]+1.0)/2.0,
-                                    min=0.0, max=1.0)
-                mask = torch.clamp((batch["mask"]+1.0)/2.0,
-                                   min=0.0, max=1.0)
-                predicted_image = torch.clamp((x_samples_ddim+1.0)/2.0,
-                                              min=0.0, max=1.0)
-
-                inpainted = (1-mask)*image+mask*predicted_image
-                inpainted = inpainted.cpu().numpy().transpose(0,2,3,1)[0]*255
-                Image.fromarray(inpainted.astype(np.uint8)).save(outpath)
+    for image, mask in tqdm(zip(images, masks)):
+        sample(
+            mask,
+            image,
+            opt.steps
+        )
